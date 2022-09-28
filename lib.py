ddcca7fd7c2d080a506c0a2285a01747cd1b0dc0
Vadim <vadimlunev1908@gmail.com>
2022-09-28 14:52:38 +0500

    alg

---
+++ lib.py
@@ -0,0 +1,9 @@
+def strangeAlg(a):
+    b = a[0]
+    c = 0
+    for i in range(1, len(a)):
+        for j in range(len(a[i])):
+            if(a[i][j] in b):
+                c+=1
+        b.append(a[i])
+    print(c)
